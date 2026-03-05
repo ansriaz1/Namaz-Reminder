e65111baf5986cbcbaa1249fea5675d804b72f3d
ansriaz1 <ansriaz93@gmail.com>
2026-03-05 06:13:55 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,41 @@
-self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open('namaz-app').then(cache => {
-            return cache.addAll([
-                'index.html',
-                'style.css',
-                'script.js',
-                'chart.js',
-                'manifest.json'
-            ]);
-        })
-    );
+const CACHE_NAME = "namaz-tracker-v3";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./chart.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-            return response || fetch(e.request);
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
+      )
+    )
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
 });
